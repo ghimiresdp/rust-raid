ac4509cece8d0ba931e44964c168c64a5a32f4f3
Sudip Ghimire <ghimiresdp@gmail.com>
2023-10-02 19:29:28 +0545

    added index trait for Series and Dataframe

--- projects/pandas/src/main.rs
+++ projects/pandas/src/main.rs
@@ -1,10 +1,37 @@
 mod pandas;
+use std::collections::HashMap;
+
 use pandas as pd;
 
 // When running this project from the directory other than the workspace
 // directory, do not forget to change the relative path below
 fn main() {
-    let df = pd::read_csv("projects/pandas/sample/students.csv", true);
-    df.describe();
-    df.head(5);
+    // creating own dataframe
+    let df1 = pd::DataFrame::from_mapping(HashMap::from_iter([
+        (
+            String::from("First Name"),
+            vec!["John", "Jane", "Satoshi", "Adam"]
+                .iter()
+                .map(|v| v.to_string())
+                .collect(),
+        ),
+        (
+            String::from("Last Name"),
+            vec!["Doe", "Deo", "Nakamoto", "Av"]
+                .iter()
+                .map(|v| v.to_string())
+                .collect(),
+        ),
+    ]));
+
+    df1.describe();
+    df1.head(5);
+    println!("Shape of the dataframe 1: {:?}", df1.shape());
+
+    // reading from a csv file
+    println!("\n\n");
+    let df2 = pd::read_csv("projects/pandas/sample/students.csv", true);
+    df2.describe();
+    df2.head(5);
+    println!("Shape of the dataframe 2: {:?}", df2.shape());
 }
--- projects/pandas/src/pandas/mod.rs
+++ projects/pandas/src/pandas/mod.rs
@@ -1,11 +1,16 @@
-use std::{collections::HashMap, fmt::Display, fs};
+use std::{
+    collections::HashMap,
+    fmt::{format, Display},
+    fs,
+    ops::Index,
+};
 
 #[derive(Debug)]
 pub(crate) enum DType {
     Bool,
     Int,
     Float,
-    String,
+    Text,
     Csv,
     Map,
 }
@@ -19,21 +24,22 @@ pub(crate) enum Cell {
     Csv(Vec<Cell>),
     Map(HashMap<String, Cell>),
 }
+impl Cell {
+    pub(crate) fn to_string(&self) -> String {
+        match self {
+            Cell::Bool(v) => v.to_string(),
+            Cell::Int(v) => v.to_string(),
+            Cell::Float(v) => v.to_string(),
+            Cell::Text(v) => v.to_string(),
+            Cell::Csv(v) => format!("CSV"),
+            Cell::Map(v) => format!("MAP"),
+        }
+    }
+}
 
 impl Display for Cell {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Cell::Bool(v) => format!("{}", v),
-                Cell::Int(v) => format!("{}", v),
-                Cell::Float(v) => format!("{}", v),
-                Cell::Text(v) => format!("{}", v),
-                Cell::Csv(v) => format!("CSV"),
-                Cell::Map(_) => format!("MAP"),
-            }
-        )
+        write!(f, "{}", self.to_string())
     }
 }
 
@@ -44,55 +50,111 @@ pub(crate) struct Header {
 }
 
 #[derive(Debug)]
+pub(crate) struct Series(Vec<Option<Cell>>);
+
+impl Index<usize> for Series {
+    type Output = Option<Cell>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl Series {
+    pub(crate) fn new(title: String, data: Vec<String>) -> Self {
+        Self(
+            data.iter()
+                .map(|v| Some(Cell::Text(v.to_string())))
+                .collect(),
+        )
+    }
+    pub(crate) fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub(crate) fn push(&mut self, element: Option<Cell>) {
+        self.0.push(element)
+    }
+}
+
+#[derive(Debug)]
 pub(crate) struct DataFrame {
     pub(crate) headers: Vec<Header>,
-    pub(crate) shape: (usize, usize),
-    pub(crate) data: HashMap<String, Vec<Option<Cell>>>,
+    pub(crate) data: HashMap<String, Series>,
+}
+
+impl Index<String> for DataFrame {
+    type Output = Series;
+    fn index(&self, index: String) -> &Self::Output {
+        self.data
+            .get(&index)
+            .unwrap_or_else(|| panic!("Wrong column name provided"))
+    }
 }
 
 impl DataFrame {
     pub(crate) fn new() -> Self {
         Self {
             headers: vec![],
-            shape: (0, 0), // (row, col)
             data: HashMap::new(),
         }
     }
+    pub(crate) fn shape(&self) -> (usize, usize) {
+        // row, col
+        let first_header = self.headers.get(0).unwrap().name.clone();
+        let _series = self.data.get(&first_header).unwrap();
+        println!("{:?}", _series);
+        (_series.len(), self.headers.len())
+    }
+    pub(crate) fn from_mapping(data: HashMap<String, Vec<String>>) -> Self {
+        let headers = (&data)
+            .keys()
+            .into_iter()
+            .map(|k| Header {
+                d_type: DType::Text,
+                name: k.to_string(),
+            })
+            .collect();
+        Self {
+            headers: headers,
+            data: data
+                .iter()
+                .map(|(k, v)| (k.to_string(), Series::new(k.to_string(), v.to_owned())))
+                .collect(),
+        }
+    }
+
     pub(crate) fn describe(&self) {
         self.headers.iter().for_each(|item| {
             println!("{:<30}: {:?}", item.name, item.d_type);
         });
     }
     pub(crate) fn head(&self, mut n: usize) {
-        if n > self.shape.0 {
-            n = self.shape.0
+        if n > self.shape().0 {
+            n = self.shape().0
         }
         let titles = self
             .headers
             .iter()
             .map(|h| format!("{:^20}", h.name))
             .collect::<Vec<String>>()
-            .join("|");
+            .join(" | ");
         println!("{titles}");
         println!("{}", "-".repeat(titles.len()));
-        // for idx in 0..n {
-        //     let x = self
-        //         .headers
-        //         .iter()
-        //         .map(|h| {
-        //             let entry = self.data.get(h.name).unwrap().clone().get(idx).unwrap();
-        //         })
-        //         .collect();
-        // }
-        // self.rows[0..n].iter().for_each(|r| {
-        //     println!(
-        //         "{}",
-        //         r.iter()
-        //             .map(|v| { format!("{:?}", v) })
-        //             .collect::<Vec<String>>()
-        //             .join("|")
-        //     );
-        // });
+        for idx in 0..n {
+            // let header = self.headers[idx].name.clone();
+
+            // let row = self
+            //     .headers
+            //     .iter()
+            //     .map(|h| match self.data.get(&h.name).unwrap()[idx] {
+            //         Some(cell) => format!("{:<20}", cell.to_string()),
+            //         None => String::from(" ".repeat(20)),
+            //     })
+            //     .collect::<Vec<String>>()
+            //     .join(" | ");
+
+            // println!("{}", row);
+        }
     }
 }
 
@@ -114,12 +176,24 @@ pub(crate) fn read_csv(path: &str, headers: bool) -> DataFrame {
                 true => name.trim().to_string(),
                 false => idx.to_string(),
             },
-            d_type: DType::String,
+            d_type: DType::Text,
         })
         .collect();
     if headers {
         lines.remove(0);
     }
+    lines.iter().for_each(|line| {
+        let row = line
+            .split(",")
+            .map(|v| v.trim().to_string())
+            .collect::<Vec<String>>();
+        if row.len() != df.headers.len() {
+            panic!("Incorrect CSV Formatting");
+        }
+        df.headers.iter().zip(row).for_each(|(header, data)| {
+            // df;
+        })
+    });
     // df.headers.iter().for_each(|h| {
     //     df.data.insert(h.name, vec![]);
     // });
@@ -130,6 +204,5 @@ pub(crate) fn read_csv(path: &str, headers: bool) -> DataFrame {
     //             .and_modify(|v| v.push(Some(Cell::Text(value.trim().to_string()))));
     //     })
     // });
-    df.shape = (lines.len(), df.headers.len());
     df
 }
