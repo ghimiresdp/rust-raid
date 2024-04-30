0f7791531fc23ccc9144620091e355e8f1e869b9
Sudip Ghimire <ghimiresdp@gmail.com>
2024-04-30 12:35:06 +0545

    code optimization

--- challenges/inter/c4_two_sum.rs
+++ challenges/inter/c4_two_sum.rs
@@ -1,20 +1,21 @@
 /// Two Sum Problem [from leetcode]
 /// https://leetcode.com/problems/two-sum/
+use std::collections::HashMap;
 
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut maps: HashMap<i32, i32> = HashMap::new();
+    for num_1 in 0..nums.len() {
+        let required = target - nums[num_1];
+        if let Some(value) = maps.get(&required) {
+            return vec![*value, num_1 as i32];
+        }
+        maps.insert(nums[num_1], num_1 as i32);
+    }
+    vec![-1, -1]
+}
 pub fn main() {
     let numbers = two_sum(vec![2, 7, 11, 15], 9);
     println!("{:?}", numbers);
-}
-
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for num_1 in 0..nums.len() {
-        for num_2 in num_1 + 1..nums.len() {
-            if nums[num_1] + nums[num_2] == target {
-                return vec![num_1 as i32, num_2 as i32];
-            }
-        }
-    }
-    vec![-1, -1]
 }
 
 #[cfg(test)]
