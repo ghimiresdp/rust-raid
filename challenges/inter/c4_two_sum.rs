3debe38aa37da6651eaa942db922fd48d9a42d78
Sudip Ghimire <ghimiresdp@gmail.com>
2024-04-30 12:11:46 +0545

    code cleanup and test cases

--- challenges/inter/c4_two_sum.rs
+++ challenges/inter/c4_two_sum.rs
@@ -1,7 +1,5 @@
-/// Two Sum Problem
+/// Two Sum Problem [from leetcode]
 /// https://leetcode.com/problems/two-sum/
-/// Given a vector of integers and an integer target, return indices of the num
-///
 
 pub fn main() {
     let numbers = two_sum(vec![2, 7, 11, 15], 9);
@@ -22,8 +20,11 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
     use super::two_sum;
+
+    #[test]
     fn test_two_sum() {
         assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
         assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(two_sum(vec![3, 2, 4], 9), vec![-1, -1]);
     }
 }
