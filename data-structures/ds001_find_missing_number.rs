dd50edd2eeb029af6a9da18c2ac02d918d9e8397
Sudip Ghimire <ghimiresdp@gmail.com>
2024-05-21 14:45:49 +0545

    Optimized solution for missing number solution

--- data-structures/ds001_find_missing_number.rs
+++ data-structures/ds001_find_missing_number.rs
@@ -5,8 +5,8 @@
  * containing distinct numbers.
  *
  * CONSTRAINTS
- * - If the array has more than 1 missing numbers, return the smallest one.
- * - The array might have n distinct 32 bit integers  (i32).
+ * - The vector should have exactly 1 missing number.
+ * - The vector should have n distinct 32 bit integers  (i32).
  * - The function should return the missing number as Option<i32>.
  * - If there are no missing numbers, the function should return None.
  *
@@ -15,8 +15,13 @@
  * - output: 6
  */
 
+/**
+ * Approach 1: Using sorting.
+ *
+ * Sorting the vector will have the time complexity of O(n log n)
+ * We first sort the number and
+ */
 fn find_missing_number(mut num: Vec<i32>) -> Option<i32> {
-    // first sort the number
     if num.len() < 2 {
         return None;
     }
@@ -29,29 +34,55 @@ fn find_missing_number(mut num: Vec<i32>) -> Option<i32> {
     None
 }
 
+/**
+ * Approach 2: calculating the expected sum and returning the difference
+ *
+ * This approach is much more performant since this approach has the time
+ * complexity of exactly O(n).
+ */
+fn find_missing_num(num: Vec<i32>) -> Option<i32> {
+    let smallest = num.iter().min().unwrap();
+    let largest = num.iter().max().unwrap();
+    let sum: i32 = num.iter().sum();
+
+    let expected_sum = ((largest * (largest + 1)) - (smallest * (smallest - 1))) / 2;
+    match expected_sum - sum {
+        0 => None,
+        s => Some(s),
+    }
+}
+
 fn main() {
     let numbers = vec![3, 7, 1, 2, 8, 4, 5];
     println!(
         "missing number in an array {:?} is: {}",
         numbers.clone(),
-        find_missing_number(numbers).unwrap_or(-1)
+        find_missing_number(numbers.clone()).unwrap_or(-1)
+    );
+    println!(
+        "missing number in an array {:?} is: {}",
+        numbers.clone(),
+        find_missing_num(numbers).unwrap_or(-1)
     )
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::find_missing_number;
+    use crate::{find_missing_num, find_missing_number};
 
     #[test]
     fn contains_missing() {
         assert_eq!(find_missing_number(vec![3, 7, 1, 2, 8, 4, 5]), Some(6));
+        assert_eq!(find_missing_num(vec![3, 7, 1, 2, 8, 4, 5]), Some(6));
     }
     #[test]
     fn contains_no_missing() {
         assert_eq!(find_missing_number(vec![3, 7, 1, 2, 8, 4, 5, 6]), None);
+        assert_eq!(find_missing_num(vec![3, 7, 1, 2, 8, 4, 5, 6]), None);
     }
     #[test]
     fn contains_no_missing_when_one_element() {
         assert_eq!(find_missing_number(vec![1]), None);
+        assert_eq!(find_missing_num(vec![1]), None);
     }
 }
