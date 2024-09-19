1dfa451385356a9352bb1cd54e25108cf8d434f7
Sudip Ghimire <ghimiresdp@gmail.com>
2024-09-20 00:45:45 +0545

    feat: convert Hex to RGB and HSL

--- problem-solving/basic/color_converter.rs
+++ problem-solving/basic/color_converter.rs
@@ -38,7 +38,7 @@ impl Color {
                 Color::HSL(h, s, l)
             }
             Color::HSL(h, s, l) => Color::HSL(h, s, l),
-            Color::HEX(hex) => todo!(),
+            Color::HEX(_) => color.to_rgb().to_hsl(),
         }
     }
     fn to_rgb(&self) -> Self {
@@ -47,15 +47,26 @@ impl Color {
         match color {
             Color::RGB(r, g, b) => Color::RGB(r, g, b),
             Color::HSL(h, s, l) => todo!(),
-            Color::HEX(hex) => todo!(),
+            Color::HEX(hex) => {
+                let (_, rgb) = hex.split_at(1);
+                println!("{:?}", rgb);
+                let rgb = u32::from_str_radix(rgb, 16).unwrap();
+                Color::RGB(
+                    (rgb / 0x10000 as u32) as u8, // 0x10000 = 0xFFFF + 0x1
+                    ((rgb % 0x10000) / (0x100)) as u8,
+                    (rgb % 0x100) as u8,
+                )
+            }
         }
     }
     fn to_hex(&self) -> Self {
         let color = self.clone();
 
         match color {
-            Color::RGB(r, g, b) => todo!(),
-            Color::HSL(h, s, l) => todo!(),
+            Color::RGB(r, g, b) => {
+                Color::HEX(format!("#{:02x}{:02x}{:02x}", r, g, b).to_uppercase())
+            }
+            Color::HSL(_, _, _) => color.to_rgb().to_hex(),
             Color::HEX(hex) => Color::HEX(hex),
         }
     }
@@ -83,8 +94,19 @@ mod tests {
     use crate::Color;
 
     #[test]
-    fn test_rgb_to_hsl() {
+    fn rgb_to_other() {
         let rgb = Color::RGB(255, 255, 255);
         assert_eq!(rgb.to_hsl(), Color::HSL(0, 0, 100));
+        assert_eq!(rgb.to_hex(), Color::HEX("#FFFFFF".to_owned()));
+
+        let rgb = Color::RGB(00, 102, 170);
+        assert_eq!(rgb.to_hex(), Color::HEX("#0066AA".to_owned()));
+    }
+
+    #[test]
+    fn hex_to_other() {
+        let hex = Color::HEX("#FFFFFF".to_owned());
+        assert_eq!(hex.to_rgb(), Color::RGB(255, 255, 255));
+        assert_eq!(hex.to_hsl(), Color::HSL(0, 0, 100));
     }
 }
