84767f95bf774fa7d29801ba919bba21a302950c
Sudip Ghimire <ghimiresdp@gmail.com>
2024-10-08 20:04:02 +0545

    feat: create vault

--- projects/vault/src/vault.rs
+++ projects/vault/src/vault.rs
@@ -10,21 +10,27 @@ use argon2::{
     password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
     Argon2,
 };
+use rand::Rng;
 use std::io::Write;
-use std::{collections::HashMap, fs::File};
+use std::{collections::HashMap, fs::OpenOptions};
+
+use crate::input;
 
 // define encoder and decoder types
 type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
 type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
 
-fn get_password_hash(password: String) -> [u8; 32] {
+fn bytes_to_hex_string(bytes: Vec<u8>) -> String {
+    bytes.iter().map(|b| format!("{b:02x}")).collect::<String>()
+}
+fn generate_salt(length: usize) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    (0..length).map(|_| rng.gen()).collect()
+}
+
+fn generate_key(password: String, salt: &Vec<u8>) -> [u8; 32] {
     let mut key = [0u8; 32]; // Can be any desired size
-    match Argon2::default().hash_password_into(
-        password.as_bytes(),
-        // salt,
-        b"test1234567890",
-        &mut key,
-    ) {
+    match Argon2::default().hash_password_into(password.as_bytes(), salt, &mut key) {
         Ok(data) => println!("{data:?}"),
         Err(error) => println!("{error:?}"),
     }
@@ -43,29 +49,69 @@ fn encrypt(key: [u8; 32], message: String) -> Vec<u8> {
     println!("{:?}", message);
     message
 }
+
+#[derive(Clone)]
 pub(crate) struct Vault {
     pub(crate) name: String,
+    key: [u8; 32],
     credentials: HashMap<String, String>,
 }
 
 impl Vault {
-    pub(crate) fn new(name: &str) -> Vault {
+    /// Create a new Vault
+    ///
+    /// the `new` method creates a new instance of `Vault` with the given name
+    /// and an empty list of credentials
+    pub(crate) fn new(name: String, key: [u8; 32]) -> Vault {
         Vault {
-            name: name.to_owned(),
+            name,
+            key,
             credentials: HashMap::new(),
         }
     }
-    pub(crate) fn open(name: &str, password: String) -> Self {
-        let key = get_password_hash(password);
+
+    /// Open an existing vault
+    ///
+    /// If a vault exists, it opens the vault, else shows an error message saying it doesn't exist.
+    pub(crate) fn open(name: String, password: String) -> Self {
+        let salt = generate_salt(20);
+        let key = generate_key(password, &salt);
         let key_string: String = key.iter().map(|k| return format!("{k:02x}")).collect();
         println!("key_string: {:?}", key_string);
 
         let encrypted = encrypt(key, String::from("Test data 111111111111111"));
         println!("Encrypted: {:?}", encrypted);
 
-        Self {
-            name: name.to_owned(),
-            credentials: HashMap::new(),
+        let vault = Self::new(name, key);
+        vault
+    }
+
+    /// Create
+    ///
+    /// This method creates a new vault with credentials.
+    /// The vault contains encrypted credentials along with few encryption data
+    /// such as number of iterations and salt.
+    pub(crate) fn create(name: String) {
+        match OpenOptions::new()
+            .write(true)
+            .create_new(true) // This will cause an error if the file already exists
+            .open(format!("{name}.vault"))
+            .as_mut()
+        {
+            Ok(file) => {
+                let password = input("Enter password:");
+                let salt = generate_salt(20);
+                let key = generate_key(password, &salt);
+
+                println!("The vault \"{name}\" has been successfully created");
+                let _ = file.write(bytes_to_hex_string(salt).as_bytes());
+                let _ = file.write(b"\n");
+                // let _ = file.writ
+            }
+            Err(e) => {
+                println!("Could not create the vault \"{name}\"");
+                println!("{e}");
+            }
         }
     }
 }
